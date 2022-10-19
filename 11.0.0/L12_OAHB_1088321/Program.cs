e2215322664ea416036b8da1b0779b8be1e9cbf5
OscarHernandezz <111389051+OscarHernandezz@users.noreply.github.com>
2022-10-19 02:43:28 +0200

    Add files via upload

---
+++ 11.0.0/L12_OAHB_1088321/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace L12_OAHB_1088321
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Puntos pn = new Puntos();
+            pn.datos();
+            pn.Imprimir();
+        }
+        class Puntos
+        {
+            private int[] notas;
+            public int suma = 0;
+            public int promedio = 0;
+            public int conteo = 0;
+            public void datos()
+            {
+                
+                notas = new int[15];
+                for (int i = 0; i < 15; i++)
+                {
+                    if (notas[i] <= 100)
+                    {
+                        Console.WriteLine("Ingrese la nota");
+                        string linea;
+                        linea = Console.ReadLine();
+                        notas[i] = int.Parse(linea);
+                        suma += notas[i];
+                        promedio = suma / 15;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ingrese una nota válida entre 0 y 100");
+                    }
+                }
+            }
+
+            public void Imprimir()
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    Console.WriteLine("La nota es: "+notas[i]);
+                }
+                Console.WriteLine("La suma de las notas es de: "+suma);
+                Console.WriteLine("La promedio de las notas es de: " + promedio);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 11.0.0/Reto1_Semana11/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Reto1_Semana11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int largo = 0;
+            int[] niveles = new int[5];
+            int suma = 0;
+            int contador = 1, contador2 = 1;
+            int mayor, nivel = 1;
+
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese la cantidad de personas en el nivel " + contador);
+                contador++;
+                niveles[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < 5; i++)
+            {
+                if (niveles[0] > niveles[1] && niveles[0] > niveles[2] && niveles[0] > niveles[3] && niveles[0] > niveles[4])
+                {
+                    mayor = niveles[0];
+                    nivel=1;
+                }
+                else if (niveles[1] > niveles[0] && niveles[1] > niveles[2] && niveles[1] > niveles[3] && niveles[1] > niveles[4] )
+                {
+                    mayor = niveles[1];
+                    nivel=2;
+                }
+                else if (niveles[2] > niveles[0] && niveles[2] > niveles[1] && niveles[2] > niveles[3] && niveles[2] > niveles[4] )
+                {
+                    mayor = niveles[2];
+                    nivel=3;
+                }
+                else if (niveles[3] > niveles[0] && niveles[3] > niveles[1] && niveles[3] > niveles[2] && niveles[3] > niveles[4] )
+                {
+                    mayor = niveles[3];
+                    nivel=4;
+                }
+                else if (niveles[4] > niveles[0] && niveles[4] > niveles[1] && niveles[4] > niveles[2] && niveles[4] > niveles[3])
+                {
+                    mayor = niveles[4];
+                    nivel = 5;
+                }
+                else
+                {
+                    mayor = 0;
+                }
+            }
+            Console.WriteLine("El nivel con más personas es: " + nivel);
+            Console.WriteLine("");
+            for (int i = 0; i < 5; i++)
+            {
+                suma += niveles[i];
+            }
+            Console.WriteLine("La cantidad de personas totales en el edificio es de: " + suma);
+            Console.ReadKey();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("La cantidad de personas en el nivel " + contador2 +" es de: " + niveles[i]);
+                contador2++;
+            }
+        }
+    }
+}
